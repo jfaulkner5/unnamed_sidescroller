da545236550570e6c4cd4f7a90b86fb7d8f4e43b
Jay <1010100@student.sae.edu.au>
2018-05-27 23:44:39 +1000

    menu controller

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    #region Variables
+
+    public GameObject PanelMain, PanelCredits, PanelOptions;
+
+
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnButtonClick(string button)
+    {
+
+    }
+
+    private void StartGame()
+    {
+
+    }
+
+    private void Options()
+    {
+
+    }
+
+    private void Credits()
+    {
+
+    }
+
+    private void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+#else
+         Application.Quit();
+#endif
+    }
+
+}
